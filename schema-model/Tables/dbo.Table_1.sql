12e8ae30cf01d2cc277bfd8bb96a794ce474f0bf
software-bvrd <89147370+software-bvrd@users.noreply.github.com>
2024-07-16 15:47:02 -0400

    first commit MC

---
+++ schema-model/Tables/dbo.Table_1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_1]
+(
+[codigo] [int] NULL,
+[descripcion12] [varchar] (50) NULL
+)
+GO
---
+++ schema-model/Tables/dbo.Table_2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_2]
+(
+[codigo1] [nchar] (10) NULL,
+[descripcion44] [nchar] (10) NULL
+)
+GO
